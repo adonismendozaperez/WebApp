c66a7b0ec17dd6d038654959b168fbaedb2c8866
Adonis <adonis.mendoza@outlook.com>
2017-05-17 12:59:45 -0400

    Agregar Asignaciones de un curso al estudiante

--- Modelo/AlumnoCurso.cs
+++ Modelo/AlumnoCurso.cs
@@ -5,6 +5,7 @@ namespace Modelo
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
+    using System.Linq;
 
     [Table("AlumnoCurso")]
     public partial class AlumnoCurso
@@ -22,11 +23,57 @@ namespace Modelo
 
         public virtual Curso Curso { get; set; }
 
+        #region Metodo de listar cursos que tiene un alumno
+        public List<AlumnoCurso> Listar()
+        {
+            var AlumnoCursos = new List<AlumnoCurso>();
+            try
+            {
+                using (var ctx = new TestContex())
+                {
+                    AlumnoCursos = ctx.AlumnoCurso.ToList();
+
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return AlumnoCursos;
+        }
+        #endregion
+
+        #region Obtener alumnocursos
+        public AlumnoCurso obtener(int id)
+        {
+            var alumnocursos = new AlumnoCurso();
+            try
+            {
+                using (var ctx = new TestContex())
+                {
+                    alumnocursos = ctx.AlumnoCurso
+                        .Where(x => x.id == id)
+                        .SingleOrDefault();
+                }
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return alumnocursos;
+        }
+        #endregion
+
+        #region Guardar
         public void Guardar()
         {
             try
             {
-                using ( var db = new TestContex())
+                using (var db = new TestContex())
                 {
                     if (this.id > 0)
                     {
@@ -45,5 +92,28 @@ namespace Modelo
                 throw;
             }
         }
+        #endregion
+
+        #region Eliminar
+        public void Eliminar()
+        {
+            try
+            {
+                using (var ctx = new TestContex())
+                {
+                    ctx.Entry(this).State = System.Data.Entity.EntityState.Deleted;
+                    ctx.SaveChanges();
+                }
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        #endregion
+
     }
 }
---
+++ WebApp/Controllers/AlumnoCursoesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Modelo;
+
+namespace WebApp.Controllers
+{
+    public class AlumnoCursoesController : Controller
+    {
+        private TestContex db = new TestContex();
+
+        // GET: AlumnoCursoes
+        public ActionResult Index()
+        {
+            var alumnoCurso = db.AlumnoCurso.Include(a => a.Alumno).Include(a => a.Curso);
+            return View(alumnoCurso.ToList());
+        }
+
+        // GET: AlumnoCursoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AlumnoCurso alumnoCurso = db.AlumnoCurso.Find(id);
+            if (alumnoCurso == null)
+            {
+                return HttpNotFound();
+            }
+            return View(alumnoCurso);
+        }
+
+        // GET: AlumnoCursoes/Create
+        public ActionResult Create()
+        {
+            ViewBag.Alumno_id = new SelectList(db.Alumno, "id", "Nombre");
+            ViewBag.Curso_id = new SelectList(db.Curso, "id", "Nombre");
+            return View();
+        }
+
+        // POST: AlumnoCursoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,Alumno_id,Curso_id,Nota")] AlumnoCurso alumnoCurso)
+        {
+            if (ModelState.IsValid)
+            {
+                db.AlumnoCurso.Add(alumnoCurso);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Alumno_id = new SelectList(db.Alumno, "id", "Nombre", alumnoCurso.Alumno_id);
+            ViewBag.Curso_id = new SelectList(db.Curso, "id", "Nombre", alumnoCurso.Curso_id);
+            return View(alumnoCurso);
+        }
+
+        // GET: AlumnoCursoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AlumnoCurso alumnoCurso = db.AlumnoCurso.Find(id);
+            if (alumnoCurso == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Alumno_id = new SelectList(db.Alumno, "id", "Nombre", alumnoCurso.Alumno_id);
+            ViewBag.Curso_id = new SelectList(db.Curso, "id", "Nombre", alumnoCurso.Curso_id);
+            return View(alumnoCurso);
+        }
+
+        // POST: AlumnoCursoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,Alumno_id,Curso_id,Nota")] AlumnoCurso alumnoCurso)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(alumnoCurso).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Alumno_id = new SelectList(db.Alumno, "id", "Nombre", alumnoCurso.Alumno_id);
+            ViewBag.Curso_id = new SelectList(db.Curso, "id", "Nombre", alumnoCurso.Curso_id);
+            return View(alumnoCurso);
+        }
+
+        // GET: AlumnoCursoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AlumnoCurso alumnoCurso = db.AlumnoCurso.Find(id);
+            if (alumnoCurso == null)
+            {
+                return HttpNotFound();
+            }
+            return View(alumnoCurso);
+        }
+
+        // POST: AlumnoCursoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            AlumnoCurso alumnoCurso = db.AlumnoCurso.Find(id);
+            db.AlumnoCurso.Remove(alumnoCurso);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
